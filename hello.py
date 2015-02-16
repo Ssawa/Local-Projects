0bc7cf5a63b0acea4a0adf2d9db9a8a8c5bba2ca
Chales DiMaggio <charles.dimaggio@coinsetter.com>
2015-02-15 23:38:29 -0500

    Update for heroku deployments

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
 	return 'Hello World!'
 
+
 if __name__ == '__main__':
-	app.run()
+	# Heroku handles the binding of ports itself and will pass
+	# us what port it will use with the $PORT enviroment variable.
+	# However, we still want to use the default port 5000 when testing
+	# locally.
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
