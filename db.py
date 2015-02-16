b3efcb9e58e62f02e473f1cc76a1e96d72e3b620
Chales DiMaggio <charles.dimaggio@coinsetter.com>
2015-02-16 16:10:32 -0500

    Begin adding of questions

--- db.py
+++ db.py
@@ -22,10 +22,36 @@ def readTokens():
             tokens = curs.fetchall()
     return tokens
 
+def readQuestions():
+    questions = None
+    with getDbConnection() as conn:
+        with conn.cursor() as curs:
+            SQL = """SELECT * FROM QUESTIONS;"""
+            curs.execute(SQL)
+            questions = curs.fetchall()
+    return questions
+
+def getQuestion(questionId):
+    question = None
+    tokens = None
+    with getDbConnection() as conn:
+        with conn.cursor() as curs:
+            SQL = """SELECT QUESTION FROM QUESTIONS WHERE ID=%s"""
+            curs.execute(SQL, questionId)
+            question = curs.fetchone()
+
+            # This is a bit of an ugly query and we should consider seeing if it can be refactored
+            SQL = """SELECT TOKEN, COALESCE(YES_VALUE, 0), COALESCE(NO_VALUE,0) FROM TOKENS tk 
+            LEFT JOIN TOKEN_QUESTION_MAP tq ON tk.ID = tq.TOKEN_ID 
+            WHERE QUESTION_ID = %s OR QUESTION_ID IS NULL;"""
+            curs.execute(SQL, questionId)
+            tokens = curs.fetchall()
+    return [question, tokens]
+
 def createToken(name):
     with getDbConnection() as conn:
         with conn.cursor() as curs:
-            SQL = """INSERT INTO TOKENS (NAME) VALUES (%s);"""
+            SQL = """INSERT INTO TOKENS (TOKEN) VALUES (%s);"""
             curs.execute(SQL, [name])
             conn.commit
 
@@ -45,6 +71,6 @@ def deleteTokens(tokenIds):
                     SQL += " OR"
                 SQL += " ID = %s"
             SQL += ';'
-            
+
             curs.execute(SQL, tokenIds)
             conn.commit
--- routes.py
+++ routes.py
@@ -3,7 +3,7 @@ import db
 
 routes = Blueprint('routes', __name__)
 
-@routes.route('/tokens', methods=['POST', 'GET'])
+@routes.route('/admin/tokens', methods=['POST', 'GET'])
 def tokens():
     if request.method == 'POST':
         name = request.form['token']
@@ -17,8 +17,16 @@ def tokens():
 # within /tokens route but currently most browsers only support forms submitting POST
 # and GET. We could have theoretically used ajax to send the DELETE request but this
 # just simplifies things for prototyping purposes
-@routes.route('/tokens/delete', methods=['POST'])
+@routes.route('/admin/tokens/delete', methods=['POST'])
 def deleteTokesn():
     tokenIds = request.form.getlist('tokenId')
     db.deleteTokens(tokenIds)
     return redirect(url_for('.tokens'))
+
+@routes.route('/admin/questions', methods=['POST', 'GET'])
+def questions():
+    return render_template('questions.html', questions=db.readQuestions())
+
+@routes.route('/admin/questions/<questionId>', methods=['POST', 'GET'])
+def question(questionId):
+    return render_template('question-single.html', question=db.getQuestion(questionId))
